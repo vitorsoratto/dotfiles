cda7bf6e22f91cb834b54841f9c8e75e756f796c
Vitor Soratto <vitorsoratto@users.noreply.github.com>
2024-04-30 10:08:32 -0300

    added intelephense

--- nvim/init.lua
+++ nvim/init.lua
@@ -483,8 +483,6 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
         pyright = {},
 
         lua_ls = {
@@ -498,6 +496,18 @@ require('lazy').setup({
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
+        intelephense = {
+          filetypes = { 'php', 'blade' },
+          settings = {
+            intelephense = {
+              filetypes = { 'php', 'blade' },
+              files = {
+                associations = { '*.blade.php', '*.php' },
+                maxSize = 5000000,
+              },
             },
           },
         },
