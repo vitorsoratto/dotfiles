0b1d32834a00c7955c65437a96b46c0327f5f776
Vitor Soratto <vitorsoratto@users.noreply.github.com>
2024-04-30 15:47:58 -0300

    ajusted for xdebug debbuging

--- nvim/lua/kickstart/plugins/debug.lua
+++ nvim/lua/kickstart/plugins/debug.lua
@@ -84,6 +84,21 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = { os.getenv('HOME') .. '/.dotfiles/nvim/vscode-php-debug/out/phpDebug.js' },
+    }
+
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003,
+      },
+    }
+
     -- Install golang specific config
     require('dap-go').setup()
   end,
